2ab903797cc073ae60905c0dd1576ee3a1016ecc
Aquiles <aquiles2ws@gmail.com>
2023-10-01 10:30:27 -0300

    arquivo python com a resolução do exercício

---
+++ python/index.py
@@ -0,0 +1,20 @@
+# Exemplos das principais tipagens de variáveis em Python.
+nome = "Achiles"
+print(type(nome)) # Tipagem (String)
+
+idade = 18
+print(type(idade)) # Tipagem (int)
+
+peso = 55.80
+print(type(peso)) # Tipagem (float)
+
+maior_de_idade = True
+print(type(maior_de_idade)) # Tipagem (Boolean)
+
+hobbies = ["Fotografia", "Estudar", "Leituras"]
+print(type(hobbies))  # Tipagem (List)
+
+
+# Solicitação de dados para um usuário
+nome_usuario = input("Por favor, digite o seu nome: ") 
+print(f"É um prazer em te conhecer, {nome_usuario}!") # Mensagem a ser imprimada.
